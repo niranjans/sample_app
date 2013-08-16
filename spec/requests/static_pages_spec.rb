93ae6f84ec505684c194498b10bddf55d8e28e71
niranjan singh <niranjan987@gmail.com>
2013-08-16 09:40:06 -0400

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
+
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
 	end
 
 
@@ -17,6 +22,12 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
+	
 	end
 
 	describe "About Page" do
@@ -24,6 +35,12 @@ describe "Static pages" do
 		it "Should have the content 'About'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+
+		
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
 
