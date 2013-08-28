c2ec2df02fa0d36c7382cdf49d34cc86ec146a5b
niranjan singh <niranjan987@gmail.com>
2013-08-28 02:05:02 -0400

    Add sign in, sign out, sessions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ describe User do
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
 
 	it { should be_valid }
 
@@ -93,6 +94,11 @@ describe User do
 	describe "with a password that's too short" do
 		before { @user.password = @user.password_confirmation = "a" * 5 }
 		it { should be_invalid }
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 
 
